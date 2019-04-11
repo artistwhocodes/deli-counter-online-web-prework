5ee073624c43707fad94bda29899d7c0bf19d735
Pamela Stacey Alvarez <artistwhocodes@gmail.com>
2019-04-11 03:17:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,18 @@
 # Write your code here.
 katz_deli = []
 
-def line(katz_deli)
-  puts "The line is currently empty."
+def line(array)
+  if array.length >= 1
+   nuarray = []
+   counter = 1
+   array.each do |name|
+     nuarray.push("#{counter}. #{name}")
+     counter += 1
+   end
+   puts "The line is currently: #{nuarray.join(" ")}"
+ else
+   puts "The line is currently empty."
+ end
 end
 
 
